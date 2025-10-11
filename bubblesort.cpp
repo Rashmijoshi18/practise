2e7a655063b1990d07390f67a7949e4b5373a09f
Rashmi Joshi <rashmijoshi3699@gmail.com>
2025-10-11 20:39:29 +0530

    Refactor bubble sort implementation and remove unused maximum element finder

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -9,20 +9,18 @@ int main()
     {
         cin >> arr[i];
     }
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        cout << arr[i];
     }
 }
--- selection_sort.cpp
+++ selection_sort.cpp
@@ -13,10 +13,10 @@ int main()
     }
 
     // Selection Sort
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
         int index = i;
-        for (int j = i + 1; j < n; j++)
+        for (int j = i; j < n; j++)
         {
             if (arr[j] < arr[index])
             {
