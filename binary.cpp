40e70dd1ef6802256fd8183525fba0e8cf895c16
Rashmi Joshi <rashmijoshi3699@gmail.com>
2025-10-12 18:45:10 +0530

    Add binary search implementation in binary.cpp

---
+++ binary.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int key;
+    cin >> key;
+
+    int start = 0;
+    int end = n - 1;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == key)
+        {
+            cout << mid << endl;
+            return 0;
+        }
+        else if (arr[mid] < key)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    cout << "Element not found." << endl;
+    return 0;
+}
