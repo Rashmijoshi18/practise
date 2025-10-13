752b116acca8bbe2c43047f92ed3e209441db22d
Rashmi Joshi <rashmijoshi3699@gmail.com>
2025-10-13 19:35:42 +0530

    Refactor delete value implementation to simplify element shifting logic

--- delete_value.cpp
+++ delete_value.cpp
@@ -6,25 +6,21 @@ int main()
     int arr[100] = {2, 4, 6, 8, 10};
     int n = 5;
     int value = 6;
-    int pos = -1;
 
-    // Find the position of the value
     for (int i = 0; i < n; i++)
     {
         if (arr[i] == value)
         {
-            pos = i;
-            break;
+            // Shift all elements to the left starting from i
+            for (int j = i; j < n - 1; j++)
+            {
+                arr[j] = arr[j + 1];
+            }
+            n--;
+            // Reduce array size
+            i--;
+            // at same index to check new element after shift
         }
-    }
-
-    if (pos != -1)
-    {
-        for (int i = pos; i < n - 1; i++)
-        {
-            arr[i] = arr[i + 1];
-        }
-        n--;
     }
 
     for (int i = 0; i < n; i++)
