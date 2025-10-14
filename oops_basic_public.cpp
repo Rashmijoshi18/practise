ecb3c6733c791c690c85a96b2b75bf4ad021b0ac
Rashmi Joshi <rashmijoshi3699@gmail.com>
2025-10-14 09:52:07 +0530

    Refactor Car class implementation to student class with updated display functionality

--- oops_basic_public.cpp
+++ oops_basic_public.cpp
@@ -1,25 +1,47 @@
+// #include <iostream>
+// using namespace std;
+
+// class Car
+// {
+// public:
+//     string brand;
+//     int year;
+
+//     void show()
+//     {
+//         cout << "Brand: " << brand << ", Year: " << year << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Car c1;
+//     // Object creation
+//     c1.brand = "Tesla";
+//     c1.year = 2024;
+
+//     c1.show();
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-
-class Car
+class student
 {
 public:
-    string brand;
-    int year;
-
-    void display()
+    string name;
+    string course;
+    int rollno;
+    void show()
     {
-        cout << "Brand: " << brand << ", Year: " << year << endl;
+        cout << "Name: " << name << ", Course: " << course << ", Roll No: " << rollno << endl;
     }
 };
-
 int main()
 {
-    Car c1;
-    // Object creation
-    c1.brand = "Tesla";
-    c1.year = 2024;
-
-    c1.display();
-    return 0;
+    student s1;
+    s1.name = " Rashmi";
+    s1.course = "Btech";
+    s1.rollno = 34;
+    s1.show();
 }
