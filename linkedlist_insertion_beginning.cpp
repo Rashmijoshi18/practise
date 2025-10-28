a1e34aa8c1fca389515d9056db9c7ec1caf24921
Rashmi Joshi <rashmijoshi3699@gmail.com>
2025-10-28 10:30:13 +0530

    Add LinkedList implementation with insertion at the beginning and traversal

---
+++ linkedlist_insertion_beginning.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class LinkedList
+{
+public:
+    int data;
+    LinkedList *next;
+};
+int main()
+{
+    LinkedList *first = new LinkedList();
+    LinkedList *second = new LinkedList();
+    LinkedList *third = new LinkedList();
+
+    first->data = 10;
+    second->data = 20;
+    third->data = 30;
+
+    first->next = second;
+    second->next = third;
+    third->next = NULL;
+
+    LinkedList *temp = first;
+    LinkedList *newnode = new LinkedList();
+    newnode->data = 4;
+    newnode->next = first;
+    first = newnode;
+
+    LinkedList *head = first;
+
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+
+    // cout<<first->data<<" ";
+    // cout<<first->next->data<<" ";
+    // cout<<first->next->next->data<<" ";
+}
