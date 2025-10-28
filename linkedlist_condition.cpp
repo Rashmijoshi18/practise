ecfa9ffad0115239efe692baeb16e6e4a6b628d2
Rashmi Joshi <rashmijoshi3699@gmail.com>
2025-10-28 10:22:16 +0530

    Add initial implementation of LinkedList class and basic search functionality

---
+++ linkedlist_condition.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class LinkedList
+{
+public:
+    int data;
+    LinkedList *next;
+};
+int main()
+{
+    LinkedList *first = new LinkedList();
+    LinkedList *second = new LinkedList();
+    LinkedList *third = new LinkedList();
+
+    first->data = 10;
+    second->data = 20;
+    third->data = 30;
+
+    first->next = second;
+    second->next = third;
+    third->next = NULL;
+
+    LinkedList *head = first;
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+
+    // cout<<first->data<<" ";
+    // cout<<first->next->data<<" ";
+    // cout<<first->next->next->data<<" ";
+}
